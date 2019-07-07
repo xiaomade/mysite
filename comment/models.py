06f0fdff662f421dc50341e4b687d6b982fcb21f
e <xiaomade@outlook.com>
2019-07-07 17:50:05 +0800

    add notification and search

--- comment/models.py
+++ comment/models.py
@@ -7,23 +7,6 @@ from django.core.mail import send_mail
 from django.conf import settings
 from django.shortcuts import render
 # Create your models here.
-class SendMail(threading.Thread):
-    def __init__(self,subject,text,email,fail_silently=False):
-        self.subject = subject
-        self.text=text
-        self.email=email
-        self.fail_silently=fail_silently
-        threading.Thread.__init__(self)
-
-    def run(self):
-        send_mail(
-            self.subject,
-            '',
-            settings.EMAIL_HOST_USER,
-            [self.email],
-            fail_silently=self.fail_silently,
-            html_message=self.text
-        )
 
 class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
@@ -38,25 +21,16 @@ class Comment(models.Model):
     parent= models.ForeignKey('self', related_name='parent_comment', null=True, on_delete=models.CASCADE)
     reply_to= models.ForeignKey(User, related_name='replies', null=True, on_delete=models.CASCADE)
 
-    def send_mail(self):
-        if self.parent is None:
-            #评论我的博客
-            subject='有人回复你的博客'
-            email=self.content_object.get_email()
-        else:
-            #回复评论
-            subject='有人回复你的评论'
-            email = self.reply_to.email
-        if email!='':
-            context={}
-            context['comment_text']=self.text
-            context['url']=self.content_object.get_url()
-            text=render(None,'comment/send_mail.html',context).content.decode('utf-8')
-            send_mail=SendMail(subject,text,email)
-            send_mail.start()
 
     def __str__(self):
         return self.text
+
+    def get_url(self):
+        return self.content_object.get_url()
+
+    def get_user(self):
+        return self.user
+
 
     class Meta:
         ordering=['comment_time']
--- mysite/views.py
+++ mysite/views.py
@@ -2,13 +2,11 @@ import datetime
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
 from django.utils import timezone
-from django.db.models import Sum
+from django.core.paginator import Paginator
+from django.db.models import Sum,Q
 from django.core.cache import cache
-
-
 from read_statistics.utils import get_seven_days_read_data,get_today_hot_data,get_yesterday_hot_data
 from blog.models import Blog
-from user.forms import LoginForm,RegForm
 
 def get_seven_days_hot_blogs():
     today=timezone.now().date()
@@ -36,4 +34,30 @@ def home(request):
     context['yesterday_hot_data'] = get_yesterday_hot_data(blog_content_type)
     context['seven_days_hot_blogs']=seven_days_hot_blogs
     return render(request,'home.html',context)
+
+def search(request):
+    search_words = request.GET.get('wd', '').strip()
+    # 分词：按空格 & | ~
+    condition = None
+    for word in search_words.split(' '):
+        if condition is None:
+            condition = Q(title__icontains=word)
+        else:
+            condition = condition | Q(title__icontains=word)
+
+    search_blogs = []
+    if condition is not None:
+        # 筛选：搜索
+        search_blogs = Blog.objects.filter(condition)
+
+    # 分页
+    paginator = Paginator(search_blogs, 20)
+    page_num = request.GET.get('page', 1)  # 获取url的页面参数（GET请求）
+    page_of_blogs = paginator.get_page(page_num)
+
+    context = {}
+    context['search_words'] = search_words
+    context['search_blogs_count'] = search_blogs.count()
+    context['page_of_blogs'] = page_of_blogs
+    return render(request, 'search.html', context)
 
